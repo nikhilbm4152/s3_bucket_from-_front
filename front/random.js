364155ffb30a8b54c7b11f09adccafedb212d92b
nikhilbm4152 <42427732+nikhilbm4152@users.noreply.github.com>
2021-10-07 16:45:19 +0530

    init

---
+++ front/random.js
@@ -0,0 +1,21 @@
+const word = "i love u till the and of the life";
+let warray = word.toLowerCase().split(" ");
+console.log(warray);
+let newArray = [];
+for (i = 0; i < warray.length; i++) {
+  if (
+    warray[i].substr(0, 1) === "a" ||
+    warray[i].substr(0, 1) === "e" ||
+    warray[i].substr(0, 1) === "i"
+  ) {
+    newArray.push(warray[i]);
+  }
+}
+console.log(newArray);
+
+// var text = "";
+// var arr = "Java Script Object Notation".split(" ");
+// for (i = 0; i < arr.length; i++) {
+//   text += arr[i].substr(0, 1);
+// }
+// console.log(text);
